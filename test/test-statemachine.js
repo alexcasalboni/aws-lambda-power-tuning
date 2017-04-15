7319e30b84321b400cda66399eb396c196b263f9
Alex Casalboni <alexsaxmib@gmail.com>
2017-04-15 21:02:27 +0200

    added statemachine tests (TODO add more!)

---
+++ test/test-statemachine.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const expect = require('expect.js');
+const fs = require('fs');
+
+describe('statemachine', function() {
+
+    describe("template.json", function() {
+        it('should be valid json', function() {
+            const template = fs.readFileSync("statemachine/template.json", 'utf8');
+            const json = JSON.parse(template);
+            expect(template).to.be.a('string');
+            expect(json).to.be.an('object');
+            expect(json.StartAt).to.be.a('string');
+            expect(json.States).to.be.an('object');
+        });
+    });
+
+    describe("template-branch.json", function() {
+        it('should be valid json', function() {
+            const template = fs.readFileSync("statemachine/template-branch.json", 'utf8');
+            const json = JSON.parse(template);
+            expect(template).to.be.a('string');
+            expect(json).to.be.an('object');
+            expect(json.StartAt).to.be.a('string');
+            expect(json.States).to.be.an('object');
+        });
+    });
+
+    describe("generate.js", function() {
+        // TODO test script or export functions
+    });
+
+});
