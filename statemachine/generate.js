d92b45d3d22e7522b7ee8f94a4a11539eaaee7de
Alex Casalboni <alexsaxmib@gmail.com>
2017-04-15 20:46:15 +0200

    Improved state machine generation (separate json files, yaml dumper for CloudFormation)

--- statemachine/generate.js
+++ statemachine/generate.js
@@ -1,12 +1,24 @@
-var program = require('commander');
+const program = require('commander');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
-// constants and default values
-const DEFAULT_POWER_VALUES = "128,192,256,320,384,448,512,576,640,704,768,832,896,960,1024,1088,1152,1216,1280,1344,1408,1472,1536";
+// function names
 const PREFIX = "aws-lambda-power-tuning-dev-";  // package name and sls env
 const INITIALIZER_FUNCTION = PREFIX + "initializer";
 const EXECUTOR_FUNCTION = PREFIX + "executor";
 const FINALIZER_FUNCTION = PREFIX + "finalizer";
 const CLEANER_FUNCTION = PREFIX + "cleaner";
+
+// state machine templates
+const MACHINE_TEMPLATE = fs.readFileSync("statemachine/template.json", 'utf8');
+const BRANCH_TEMPLATE = fs.readFileSync("statemachine/template-branch.json", 'utf8');
+
+
+// YAML config (serverles.hml)
+const SERVERLESS_YAML_FILENAME = './serverless.yml';
+var serverlessYaml = yaml.safeLoad(fs.readFileSync(SERVERLESS_YAML_FILENAME, 'utf8'));
+const DEFAULT_POWER_VALUES = serverlessYaml.provider.environment.powerValues;
+
 
 // program definition
 program
@@ -14,98 +26,51 @@ program
     .option('-R, --region <REGION>', 'The AWS Region name')
     .option('-A, --account <ACCOUNT_ID>', 'Your AWS Account ID')
     .action(function(REGION, ACCOUNT_ID) {
-        const powerValues = DEFAULT_POWER_VALUES.split(',');  // TODO optional param?
-        const stateMachine = buildStateMachine(REGION, ACCOUNT_ID, powerValues);
-        console.log(stateMachine);
+
+        const stateMachineStr = buildStateMachine(REGION, ACCOUNT_ID);
+
+        // edit state machine definition
+        var lambdaPowerStateMachine = serverlessYaml.resources.Resources.LambdaPowerStateMachine;
+        lambdaPowerStateMachine.Properties.DefinitionString = stateMachineStr;
+        
+        // write back to yaml file
+        const newYaml = yaml.safeDump(serverlessYaml, {lineWidth: 999999});
+        fs.writeFileSync(SERVERLESS_YAML_FILENAME, newYaml)
+
     })
     .parse(process.argv);
 
 
-function buildStateMachine (region, account, powerValues) {
+function buildStateMachine (REGION, ACCOUNT_ID, powerValues) {
 
-    var machineTemplate = stateMachineTemplate();
+    // TODO add optional param?
+    powerValues = powerValues || DEFAULT_POWER_VALUES.split(',')
+
+    var machineTemplate = JSON.parse(MACHINE_TEMPLATE),
+        branches = machineTemplate.States.Branching.Branches,
+        vars = {'REGION': REGION, 'ACCOUNT_ID': ACCOUNT_ID};
 
     powerValues.forEach(function(value) {
-
-        machineTemplate.States.Branching.Branches.push(createBranch(value));
-
+        branches.push(createBranch(value, vars));
     });
 
-    return JSON.stringify(machineTemplate)
-        .replace(/\{REGION\}/g, region)
-        .replace(/\{ACCOUNT_ID\}/g, account);
-
+    return applyTemplateVars(JSON.stringify(machineTemplate), vars);
 }
 
-
-function createBranch (num) {
-    const branch = branchTemplate();
-    const branchStr = JSON.stringify(branch).replace(/\{num\}/g, num);
-    return JSON.parse(branchStr);
+function createBranch (num, vars) {
+    vars.NUM = num;
+    const branch = applyTemplateVars(BRANCH_TEMPLATE, vars);
+    return JSON.parse(branch);
 }
 
-
-function stateMachineTemplate () {
-    return {
-      "Comment": "Step Functions state machine generator for AWS Lambda Power Tuning",
-      "StartAt": "Initializer",
-      "States": {
-        "Initializer": {
-          "Type": "Task",
-          "Resource": "arn:aws:lambda:{REGION}:{ACCOUNT_ID}:function:" + INITIALIZER_FUNCTION,
-          "Next": "Branching"
-        },
-        "Branching": {
-          "Type": "Parallel",
-          "Next": "After Branching",
-          "Branches": []  // will be filled w/ one branch for each power value
-        },
-        "After Branching": {
-          "Type": "Parallel",
-          "End": true,
-          "Branches": [
-            {
-              "StartAt": "Finalizer",
-              "States": {
-                "Finalizer": {
-                  "Type": "Task",
-                  "Resource": "arn:aws:lambda:{REGION}:{ACCOUNT_ID}:function:" + FINALIZER_FUNCTION,
-                  "End": true
-                }
-              }
-            },
-            {
-              "StartAt": "Cleaner",
-              "States": {
-                "Cleaner": {
-                  "Type": "Task",
-                  "Resource": "arn:aws:lambda:{REGION}:{ACCOUNT_ID}:function:" + CLEANER_FUNCTION,
-                  "End": true
-                }
-              }
-            }
-          ]
-        }
-      }
-    };
-}
-
-
-function branchTemplate () {
-    return {
-      "StartAt": "{num}MB",
-      "States": {
-        "{num}MB": {
-          "Type": "Pass",
-          "Result": "128",
-          "ResultPath": "$.num",
-          "Next": "{num}MB Executor"
-        },
-        "{num}MB Executor": {
-          "Type": "Task",
-          "Resource": "arn:aws:lambda:{REGION}:{ACCOUNT_ID}:function:" + EXECUTOR_FUNCTION,
-          "End": true
-        }
-      }
-    };
+function applyTemplateVars (template, vars) {
+    return template
+        .replace(/\{REGION\}/g, vars.REGION)
+        .replace(/\{ACCOUNT_ID\}/g, vars.ACCOUNT_ID)
+        .replace(/\{NUM\}/g, vars.NUM)
+        .replace(/\{INITIALIZER_FUNCTION\}/g, INITIALIZER_FUNCTION)
+        .replace(/\{EXECUTOR_FUNCTION\}/g, EXECUTOR_FUNCTION)
+        .replace(/\{CLEANER_FUNCTION\}/g, CLEANER_FUNCTION)
+        .replace(/\{FINALIZER_FUNCTION\}/g, FINALIZER_FUNCTION)
+    ;
 }
