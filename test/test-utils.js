69e5951576bb23b3161b83b8a6d13d1e6ca1f795
Alex Casalboni <alexsaxmib@gmail.com>
2019-07-21 12:10:12 -0700

    updated tests too

--- test/test-utils.js
+++ test/test-utils.js
@@ -13,9 +13,10 @@ AWS.mock('Lambda', 'createAlias', {});
 AWS.mock('Lambda', 'deleteAlias', {});
 AWS.mock('Lambda', 'invoke', {});
 
-describe('Lambda Utils', function () {
+describe('Lambda Utils', () => {
 
-    const promises = [
+    // I'm dynamically generating tests for all these utilities
+    const lambdaUtilities = [
         utils.checkLambdaAlias,
         utils.setLambdaPower,
         utils.publishLambdaVersion,
@@ -25,6 +26,7 @@ describe('Lambda Utils', function () {
         utils.invokeLambda,
     ];
 
+    // just returns the utility name for convenience
     function _fname(func) {
         const keys = Object.keys(utils);
         for (var i = 0; i < keys.length; i++) {
@@ -36,55 +38,47 @@ describe('Lambda Utils', function () {
         throw new Error('Export not found! ' + func);
     }
 
-    promises.forEach(function (func) {
-        describe(_fname(func), function () {
-            it('should return a promise', function () {
-                var res = func.bind(null, 'arn:aws:lambda:us-east-1:XXX:function:YYY', 'test', 'test');
-                expect(res()).to.be.a(Promise);
+    lambdaUtilities.forEach(func => {
+        describe(_fname(func), () => {
+            it('should return a promise', () => {
+                const result = func('arn:aws:lambda:us-east-1:XXX:function:YYY', 'test', 'test');
+                expect(result).to.be.a(Promise);
             });
             // TODO add more tests!
         });
     });
 
-    describe('extractDuration', function () {
+    describe('extractDuration', () => {
         const log =
             'START RequestId: 55bc566d-1e2c-11e7-93e6-6705ceb4c1cc Version: $LATEST\n' +
             'END RequestId: 55bc566d-1e2c-11e7-93e6-6705ceb4c1cc\n' +
             'REPORT RequestId: 55bc566d-1e2c-11e7-93e6-6705ceb4c1cc\tDuration: 469.40 ms\tBilled Duration: 500 ms\tMemory Size: 1024 MB\tMax Memory Used: 21 MB'
             ;
-        it('should extract the duration from a Lambda log', function () {
+        it('should extract the duration from a Lambda log', () => {
             expect(utils.extractDuration(log)).to.be(469.40);
         });
-        it('should return 0 if duration is not found', function () {
+        it('should return 0 if duration is not found', () => {
             expect(utils.extractDuration('hello world')).to.be(0);
             const partialLog = 'START RequestId: 55bc566d-1e2c-11e7-93e6-6705ceb4c1cc Version: $LATEST\n';
             expect(utils.extractDuration(partialLog)).to.be(0);
         });
     });
 
-    describe('computeStats', function () {
+    describe('computeStats', () => {
         const minCost = 0.000000208;  // $
         const minRAM = 128;  // MB
         const value = 1024;  // MB
         const averageDuration = 300;  //ms
 
-        it('should return a promise', function () {
-            const price = utils.computeStats(minCost, minRAM, value, averageDuration);
-            expect(price).to.be.a(Promise);
-        });
-        it('should return the average price', function () {
-            const statsFunc = utils.computeStats.bind(null, minCost, minRAM, value, averageDuration);
-            return Promise.resolve()
-                .then(statsFunc)
-                .then(function (stats) {
-                    expect(stats.averagePrice).to.be(minCost * 8 * 3);
-                    expect(stats.averageDuration).to.be(300);
-
-                });
+        it('should return the average price', () => {
+            const stats = utils.computeStats(minCost, minRAM, value, averageDuration);
+            expect(stats).to.be.an('object');
+            expect(stats.averagePrice).to.be(minCost * 8 * 3);
+            expect(stats.averageDuration).to.be(300);
         });
     });
 
-    describe('computeAverageDuration', function () {
+    describe('computeAverageDuration', () => {
         const results = [
             // 1s (will be discarted)
             { 'StatusCode': 200, 'LogResult': 'U1RBUlQgUmVxdWVzdElkOiA0NzlmYjUxYy0xZTM4LTExZTctOTljYS02N2JmMTYzNjA4ZWQgVmVyc2lvbjogOTkKMjAxNy0wNC0xMFQyMTo1NDozMi42ODNaCTQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAl2YWx1ZTEgPSB1bmRlZmluZWQKMjAxNy0wNC0xMFQyMTo1NDozMi42ODNaCTQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAl2YWx1ZTIgPSB1bmRlZmluZWQKMjAxNy0wNC0xMFQyMTo1NDozMi42ODNaCTQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAl2YWx1ZTMgPSB1bmRlZmluZWQKRU5EIFJlcXVlc3RJZDogNDc5ZmI1MWMtMWUzOC0xMWU3LTk5Y2EtNjdiZjE2MzYwOGVkClJFUE9SVCBSZXF1ZXN0SWQ6IDQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAlEdXJhdGlvbjogMS4wIG1zCUJpbGxlZCBEdXJhdGlvbjogMTAwIG1zIAlNZW1vcnkgU2l6ZTogMTI4IE1CCU1heCBNZW1vcnkgVXNlZDogMTUgTUIJCg==', 'Payload': 'null' },
@@ -98,62 +92,51 @@ describe('Lambda Utils', function () {
             { 'StatusCode': 200, 'LogResult': 'U1RBUlQgUmVxdWVzdElkOiA0NzlmYjUxYy0xZTM4LTExZTctOTljYS02N2JmMTYzNjA4ZWQgVmVyc2lvbjogOTkKMjAxNy0wNC0xMFQyMTo1NDozMi42ODNaCTQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAl2YWx1ZTEgPSB1bmRlZmluZWQKMjAxNy0wNC0xMFQyMTo1NDozMi42ODNaCTQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAl2YWx1ZTIgPSB1bmRlZmluZWQKMjAxNy0wNC0xMFQyMTo1NDozMi42ODNaCTQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAl2YWx1ZTMgPSB1bmRlZmluZWQKRU5EIFJlcXVlc3RJZDogNDc5ZmI1MWMtMWUzOC0xMWU3LTk5Y2EtNjdiZjE2MzYwOGVkClJFUE9SVCBSZXF1ZXN0SWQ6IDQ3OWZiNTFjLTFlMzgtMTFlNy05OWNhLTY3YmYxNjM2MDhlZAlEdXJhdGlvbjogMy4wIG1zCUJpbGxlZCBEdXJhdGlvbjogMTAwIG1zIAlNZW1vcnkgU2l6ZTogMTI4IE1CCU1heCBNZW1vcnkgVXNlZDogMTUgTUIJCg==', 'Payload': 'null' },
         ];
 
-        it('should return a promise', function () {
+        it('should return the average price', () => {
             const duration = utils.computeAverageDuration(results);
-            expect(duration).to.be.a(Promise);
+            expect(duration).to.be(2);
         });
-        it('should return the average price', function () {
-            const durationFunc = utils.computeAverageDuration.bind(null, results);
-            return Promise.resolve()
-                .then(durationFunc)
-                .then(function (duration) {
-                    expect(duration).to.be(2);
-                });
-        });
-        it('should return 0 if empty results', function () {
-            const durationFunc = utils.computeAverageDuration.bind(null, []);
-            return Promise.resolve()
-                .then(durationFunc)
-                .then(function (duration) {
-                    expect(duration).to.be(0);
-                });
+        it('should return 0 if empty results', () => {
+            const duration = utils.computeAverageDuration([]);
+            expect(duration).to.be(0);
         });
     });
 
-    describe('base64decode', function () {
-        it('should convert a string to base64', function () {
+    describe('base64decode', () => {
+        it('should convert a string to base64', () => {
             expect(utils.base64decode('aGVsbG8gd29ybGQ=')).to.be('hello world');
             expect(utils.base64decode('bG9yZW0gaXBzdW0=')).to.be('lorem ipsum');
         });
-        it('should explode with non-string arguments', function () {
-            expect(utils.base64decode.bind(null, null)).to.throwError();
-            expect(utils.base64decode.bind(null, undefined)).to.throwError();
-            expect(utils.base64decode.bind(null, 10)).to.throwError();
+        it('should explode with non-string arguments', () => {
+            expect(() => utils.base64decode(null)).to.throwError();
+            expect(() => utils.base64decode(undefined)).to.throwError();
+            expect(() => utils.base64decode(10)).to.throwError();
         });
     });
 
-    describe('range', function () {
-        it('should generate a list of size N', function () {
+    describe('range', () => {
+        it('should generate a list of size N', () => {
+            expect(utils.range(1)).to.be.an('array');
             expect(utils.range(0)).to.have.length(0);
             expect(utils.range(5)).to.have.length(5);
             expect(utils.range(50)).to.have.length(50);
             expect(utils.range(500)).to.have.length(500);
         });
-        it('should explode when called with invalid arguments', function () {
-            [-1, -2, -Infinity, Infinity, null, undefined].forEach(function (val) {
-                expect(utils.range.bind(null, val)).to.throwError();
+        it('should explode when called with invalid arguments', () => {
+            [-1, -2, -Infinity, Infinity, null, undefined].forEach(val => {
+                expect(() => utils.range(val)).to.throwError();
             });
         });
     });
 
-    describe('lambdaClientFromARN', function () {
-        it('should return the region name', function () {
+    describe('lambdaClientFromARN', () => {
+        it('should return the region name', () => {
             arn = "arn:aws:lambda:us-east-1:XXX:function:YYY"
             expect(utils.lambdaClientFromARN(arn).config.region).to.be('us-east-1');
     });
-        [undefined, null, 0, 10, "", "arn:aws", {}].forEach(function (arn) {
-            it('should explode when called with "' + arn + '"', function () {
-                expect(utils.lambdaClientFromARN.bind(null, arn)).to.throwError();
+        [undefined, null, 0, 10, "", "arn:aws", {}].forEach(arn => {
+            it('should explode when called with "' + arn + '"', () => {
+                expect(() => utils.lambdaClientFromARN(arn)).to.throwError();
             });
         });
     });
