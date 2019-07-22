0ff6fe6627b7686c51c46bd17c676bf3a33dd50a
Alex Casalboni <alexsaxmib@gmail.com>
2019-07-21 18:57:54 -0700

    added missing value

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # config
 BUCKET_NAME=your-sam-templates-bucket
 STACK_NAME=lambda-power-tuning
-PowerValues='128,512,1024,1536,3008'
+PowerValues='128,256,512,1024,1536,3008'
 
 # package
 sam package --s3-bucket $BUCKET_NAME --template-file template.yml --output-template-file packaged.yml
