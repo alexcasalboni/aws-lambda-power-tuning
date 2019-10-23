b2d7c38e0ed68d1e8fb67ba25597a2b2556e340f
Alex Casalboni <alexsaxmib@gmail.com>
2019-10-24 00:27:51 +0200

    implement new optimizer function

---
+++ lambda/optimizer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const utils = require('./utils');
+
+/**
+ * Optionally auto-optimize based on the optimal power value.
+ */
+module.exports.handler = async(event, context) => {
+
+    const {lambdaARN, analysis, autoOptimize, autoOptimizeAlias} = event;
+
+    const optimalValue = analysis.power;
+    
+    validateInput(lambdaARN, optimalValue); // may throw
+
+    if (!autoOptimize) {
+        return console.log("Not optimizing");
+    }
+
+    if (!autoOptimizeAlias) {
+        // only update $LATEST power
+        await utils.setLambdaPower(lambdaARN, optimalValue);
+    } else {
+        // create/update alias
+        await utils.createPowerConfiguration(lambdaARN, optimalValue, autoOptimizeAlias);
+    }
+
+    return "OK";    
+};
+
+const validateInput = (lambdaARN, optimalValue) => {
+    if (!lambdaARN) {
+        throw new Error('Missing or empty lambdaARN');
+    }
+    if (!optimalValue) {
+        throw new Error('Missing or empty optimal value');
+    }
+};
