92bc58eb89d40246432ad7ee0ddcb0f5a06046c5
Alex Casalboni <alexsaxmib@gmail.com>
2019-10-24 19:07:40 +0200

    add security section to readme, update deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 BUCKET_NAME=your-sam-templates-bucket
 STACK_NAME=lambda-power-tuning
 PowerValues='128,256,512,1024,1536,3008'
+LambdaResource='*'
 
 # package
 sam package --s3-bucket $BUCKET_NAME --template-file template.yml --output-template-file packaged.yml
 
 # deploy
-sam deploy --template-file packaged.yml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --parameter-overrides PowerValues=$PowerValues
+sam deploy --template-file packaged.yml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --parameter-overrides PowerValues=$PowerValues lambdaResource=$LambdaResource
