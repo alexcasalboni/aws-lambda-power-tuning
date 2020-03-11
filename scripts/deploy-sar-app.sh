96dc4803c59e941599efcd26fdb42845ef8349a5
Alex Casalboni <alexsaxmib@gmail.com>
2020-03-11 15:57:33 +0100

    bugfix (optimizer with dry-run shouldn't validate input)

--- scripts/deploy-sar-app.sh
+++ scripts/deploy-sar-app.sh
@@ -1,9 +1,9 @@
 # config
-BUCKET_NAME=your-sam-templates-bucket
-STACK_NAME=lambda-power-tuning-app
+BUCKET_NAME=sam-templates-demos-dublin
+STACK_NAME=lambda-power-tuning-app-2
 
 # package
-sam package --s3-bucket $BUCKET_NAME --template-file deploy-sar.yml --output-template-file packaged-sar.yml
+sam package --s3-bucket $BUCKET_NAME --template-file scripts/deploy-sar-app.yml --output-template-file packaged-sar.yml
 
 # deploy
 sam deploy --template-file packaged-sar.yml --stack-name $STACK_NAME --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM
