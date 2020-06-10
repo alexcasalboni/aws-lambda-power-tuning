4b3c4958b497a8224dcae8121020d4f9a5574b65
Alex Casalboni <alexsaxmib@gmail.com>
2020-06-10 13:01:00 +0200

    improve invocation logging

--- lambda/utils.js
+++ lambda/utils.js
@@ -214,7 +214,7 @@ module.exports.invokeLambdaWithProcessors = async(lambdaARN, alias, payload, pre
  * Invoke a given Lambda Function:Alias with payload and return its logs.
  */
 module.exports.invokeLambda = (lambdaARN, alias, payload) => {
-    console.log('Invoking alias ', alias);
+    console.log(`Invoking function ${lambdaARN}${alias?(':'+alias):''} with payload ${JSON.stringify(payload)}`);
     const params = {
         FunctionName: lambdaARN,
         Qualifier: alias,
