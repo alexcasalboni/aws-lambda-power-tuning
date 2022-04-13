a0bcc9938cd26f739df3f681cb38f21f2aff77d2
ldcorentin <ledevedeccorentin@gmail.com>
2022-04-13 14:10:01 +0200

    Add permissions boundary option in roles

--- terraform/module/variables.tf
+++ terraform/module/variables.tf
@@ -6,3 +6,7 @@ variable "lambda_function_prefix" {
   default = "lambda_power_tuning"
 }
 
+variable "permissions_boundary" {
+  default     = null
+  description = "ARN of the policy that is used to set the permissions boundary for the role."
+}
