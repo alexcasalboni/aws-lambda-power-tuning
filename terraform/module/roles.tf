5baf63cec184225ac3fcd0cb8a8119b9aad6c3e6
Alex Casalboni <alex@alexcasalboni.com>
2022-05-18 15:57:26 +0200

    Consistent indentation

--- terraform/module/roles.tf
+++ terraform/module/roles.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "analyzer_role" {
-  name               = "${var.lambda_function_prefix}-analyzer_role"
+  name                 = "${var.lambda_function_prefix}-analyzer_role"
   permissions_boundary = var.permissions_boundary
-  path               = "/${var.lambda_function_prefix}/"
-  assume_role_policy = file("${path.module}/json_files/lambda.json")
+  path                 = "/${var.lambda_function_prefix}/"
+  assume_role_policy   = file("${path.module}/json_files/lambda.json")
 }
 
 resource "aws_iam_role" "optimizer_role" {
-  name               = "${var.lambda_function_prefix}-optimizer_role"
+  name                 = "${var.lambda_function_prefix}-optimizer_role"
   permissions_boundary = var.permissions_boundary
-  path               = "/${var.lambda_function_prefix}/"
-  assume_role_policy = file("${path.module}/json_files/lambda.json")
+  path                 = "/${var.lambda_function_prefix}/"
+  assume_role_policy   = file("${path.module}/json_files/lambda.json")
 }
 
 resource "aws_iam_role" "executor_role" {
-  name               = "${var.lambda_function_prefix}-executor_role"
+  name                 = "${var.lambda_function_prefix}-executor_role"
   permissions_boundary = var.permissions_boundary
-  path               = "/${var.lambda_function_prefix}/"
-  assume_role_policy = file("${path.module}/json_files/lambda.json")
+  path                 = "/${var.lambda_function_prefix}/"
+  assume_role_policy   = file("${path.module}/json_files/lambda.json")
 }
 
 resource "aws_iam_role" "initializer_role" {
