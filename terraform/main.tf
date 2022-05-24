ba6ae5f6ec78a4b773bb3039d9fca0336db204ab
Andrew Kay <andrewkay@C02FLCCBMD6M.local>
2022-05-24 09:52:18 +0100

    Removing the region as it was reported to have broken things

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,4 @@
 module "power_tuning" {
   source     = "./module"
-  aws_region = var.aws_region
   account_id = var.account_id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,3 @@
-variable "aws_region" {
-  default = "eu-west-1"
-}
-
 variable "account_id" {
-  default = "123456789101"
+  default = "411140388634"
 }
