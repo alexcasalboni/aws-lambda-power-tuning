abc3894d1bcdae3e7c327803d668b1fe90703eaa
ldcorentin <ledevedeccorentin@gmail.com>
2022-06-30 08:59:46 +0200

    Add variable role_path_prefix to override lambda_function_prefix variable for the path role

--- terraform/module/variables.tf
+++ terraform/module/variables.tf
@@ -6,6 +6,12 @@ variable "lambda_function_prefix" {
   default = "lambda_power_tuning"
 }
 
+variable "role_path_prefix" {
+  default     = null
+  type        = string
+  description = "If you want to have a role path different from the lambda_function_prefix, use this variable."
+}
+
 variable "permissions_boundary" {
   default     = null
   description = "ARN of the policy that is used to set the permissions boundary for the role."
