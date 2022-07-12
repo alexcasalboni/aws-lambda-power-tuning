3cbb52c3ad7b4e94f76f37ef3aad028c66e6890b
ldcorentin <ledevedeccorentin@gmail.com>
2022-07-12 13:18:43 +0200

    Revert added spaces

--- terraform/module/locals.tf
+++ terraform/module/locals.tf
@@ -1,7 +1,7 @@
 locals {
   defaultPowerValues = "[128,256,512,1024,1536,3008]"
   minRAM             = 128
-  baseCosts          = jsonencode({ "x86_64" : { "ap-east-1" : 2.9e-9, "af-south-1" : 2.8e-9, "me-south-1" : 2.6e-9, "eu-south-1" : 2.4e-9, "ap-northeast-3" : 2.7e-9, "default" : 2.1e-9 }, "arm64" : { "default" : 1.7e-9 } })
+  baseCosts          = jsonencode({"x86_64": {"ap-east-1":2.9e-9,"af-south-1":2.8e-9,"me-south-1":2.6e-9,"eu-south-1":2.4e-9,"ap-northeast-3":2.7e-9,"default":2.1e-9}, "arm64": {"default":1.7e-9}})
   sfCosts            = jsonencode({ "default" : 0.000025, "us-gov-west-1" : 0.00003, "ap-northeast-2" : 0.0000271, "eu-south-1" : 0.00002625, "af-south-1" : 0.00002975, "us-west-1" : 0.0000279, "eu-west-3" : 0.0000297, "ap-east-1" : 0.0000275, "me-south-1" : 0.0000275, "ap-south-1" : 0.0000285, "us-gov-east-1" : 0.00003, "sa-east-1" : 0.0000375 })
   visualizationURL   = "https://lambda-power-tuning.show/"
 
