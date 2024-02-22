f6b4076a0a4a6fe25f9709464782bef1f68370dc
André Stoll <andre.stoll@hotmail.com>
2024-02-22 17:33:19 +0100

    Folder structure changes and added tests

---
+++ cdk/csharp/README.md
@@ -0,0 +1,28 @@
+# How to deploy the AWS Lambda Power Tuner tool using the CDK for C#
+
+This CDK project deploys the *AWS Lambda Power Tuner* tool.
+
+You can use the project as a standalone or reuse it within your own CDK projects.
+
+## Prerequisites
+
+- [AWS CDK Toolkit](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html#getting_started_install)
+- [.NET 8.0 or later](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html#getting_started_prerequisites)
+
+See also the general [prerequisites for CDK](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html#getting_started_prerequisites) projects.
+
+## Deployment
+
+```
+cd cdk/csharp
+dotnet build src
+cdk deploy
+```
+
+## Useful commands
+
+* `dotnet build src` compile this app
+* `dotnet test src`  test this app
+* `cdk deploy`       deploy this stack to your default AWS account/region
+* `cdk diff`         compare deployed stack with current state
+* `cdk synth`        emits the synthesized CloudFormation template
---
+++ cdk/csharp/src/tests/Cdk.Tests/TheLambdaPowerTunerStackTest.cs
@@ -0,0 +1,36 @@
+using Amazon.CDK;
+using Amazon.CDK.Assertions;
+
+using ObjectDict = System.Collections.Generic.Dictionary<string, object>;
+
+namespace TheLambdaPowerTunerStack.Tests
+{
+    [TestClass]
+    public class TheLambdaPowerTunerStackTest
+    {
+        private Template? _template;
+
+        [TestInitialize()]
+        public void Startup()
+        {
+            var app = new App();
+            var stack = new TheLambdaPowerTunerStack(app, "TheLambdaPowerTunerStack", new StackProps());
+            _template = Template.FromStack(stack);
+        }
+
+        [TestMethod]
+        public void TestSar()
+        {
+            _template?.HasResourceProperties("AWS::Serverless::Application", new ObjectDict
+                {
+                    { "Location" , new ObjectDict
+                        {
+                            { "ApplicationId", "arn:aws:serverlessrepo:us-east-1:451282441545:applications/aws-lambda-power-tuning" },
+                            { "SemanticVersion", "4.3.0" }
+                        }
+                    }
+                } 
+            );
+        }
+    }
+}
