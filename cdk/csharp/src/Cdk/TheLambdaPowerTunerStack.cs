cca5ee4019e588e4c2d9efaf04a12cdc3162bd01
André Stoll <andre.stoll@hotmail.com>
2024-02-29 12:04:37 +0100

    Updated semantic version

--- cdk/csharp/src/Cdk/TheLambdaPowerTunerStack.cs
+++ cdk/csharp/src/Cdk/TheLambdaPowerTunerStack.cs
@@ -29,7 +29,7 @@ namespace TheLambdaPowerTunerStack
                 {
                     Location = new CfnApplication.ApplicationLocationProperty {
                         ApplicationId = "arn:aws:serverlessrepo:us-east-1:451282441545:applications/aws-lambda-power-tuning",
-                        SemanticVersion = "4.3.0"
+                        SemanticVersion = "4.3.4"
                     },
                     Parameters = stateMachineConfiguration
                 }
