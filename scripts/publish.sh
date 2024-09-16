9d0267345d3ccc01013c7430e76ec8faa4cd7f49
Alex Casalboni <alexsaxmib@gmail.com>
2024-09-16 11:17:08 +0200

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,6 @@
 
 S3_BUCKET=alex-casalboni-apps
 
-sam package --output-template-file packaged.yaml --s3-bucket $S3_BUCKET
+sam package --template-file template.yml --output-template-file packaged.yml --s3-bucket $S3_BUCKET --region us-east-1
 
-sam publish --template packaged.yaml --region us-east-1
+sam publish --template packaged.yml --region us-east-1
